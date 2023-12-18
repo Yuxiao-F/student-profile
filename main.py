6b29b0a9924322f8da7ede017b6b7a484397b7cf
Yuxiao <yuxiao@YuxiaodeAir.fios-router.home>
2023-12-18 02:27:32 -0500

    version 4

--- main.py
+++ main.py
@@ -20,9 +20,8 @@ async def update_profile(request: Request, uni: str, interest: str = Form(...), 
     new_content = [uni, interest, schedule]
     # print(new_content)
     ProfileResource.update_account(new_content)
-    result = ProfileResource.get_profile_by_uni(uni)
     # return {"message": f"Profile with uni: {uni} updated successfully"}
-    return templates.TemplateResponse("profile.html", {"request": request, "user_info": result})
+    return templates.TemplateResponse("profile.html", {"request": request, "uni": uni})
 
 
 @app.post("/create_profile/{uni}")
@@ -31,7 +30,7 @@ def create_profile(request: Request, uni: str, name: str = Form(...), interest: 
     # print(new_content)
     ProfileResource.create_account(new_content)
     result = ProfileResource.get_profile_by_uni(uni)
-    return templates.TemplateResponse("profile.html", {"request": request, "user_info": result})
+    return templates.TemplateResponse("profile.html", {"request": request, "uni": uni})
     # return {"message": "Profile created successfully"}
 
 
@@ -39,7 +38,7 @@ def create_profile(request: Request, uni: str, name: str = Form(...), interest: 
 @app.post("/delete_profile/{uni}")
 async def delete_profile(request: Request, uni: str):
     ProfileResource.delete_profile_by_uni(uni)
-    return templates.TemplateResponse("create_profile.html", {"request": request})
+    return templates.TemplateResponse("create_profile.html", {"request": request, "uni": uni})
     # return {"message": f"Profile with uni: {uni} deleted successfully"}
 
 
